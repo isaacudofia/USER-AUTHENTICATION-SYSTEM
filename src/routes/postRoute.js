11fddaee33914c9b9f76a6fbd176b221c63e89fb
Isaac Udofia <isaacudofia0@gmail.com>
2025-05-17 15:03:19 +0100

    Update routes

--- src/routes/postRoute.js
+++ src/routes/postRoute.js
@@ -10,8 +10,8 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/posts", authMiddleware, getAllPosts);
-router.get("/post/:id", authMiddleware, getPost);
+router.get("/posts", getAllPosts);
+router.get("/post/:id", getPost);
 router.post("/post", authMiddleware, addPost);
 router.put("/post/:id", authMiddleware, updatePost);
 router.delete("/post/:id", authMiddleware, deletePost);
